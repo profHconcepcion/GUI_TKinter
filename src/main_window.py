69d76fb0f5860f034d9735ff47f80869f4946103
hconcepcion <hector.concepcion@cooperton.com>
2025-07-10 19:17:35 -0400

    adding two labels

--- src/main_window.py
+++ src/main_window.py
@@ -9,10 +9,14 @@ window.title('Ventana Principal') # Titulo de la pantalla
 window.geometry('300x200') # Dimensiones de la pantalla
 
 
-lb_header = tk.Label(window, text='Titulo principal de la pantalla')
+lb_header = tk.Label(window, text='Primer Label')
 lb_header.pack()
 
+lb_sub_header = tk.Label(window, text='Segundo label')
+lb_sub_header.pack()
 
+lb_sub_sub_header = tk.Label(window, text='Tercer Label')
+lb_sub_sub_header.pack()
 
 window.mainloop()
 
